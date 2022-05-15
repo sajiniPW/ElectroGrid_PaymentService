a7edb020bbecf23a2c22bab98cbe3f9dd695c958
sajiniPW <it20418274@my.sliit.lk>
2022-05-15 10:52:36 +0530

    create Insert method

--- src/com/Payment.java
+++ src/com/Payment.java
@@ -24,5 +24,47 @@ public class Payment {
 	} 
 	
 
+	//Insert payment detail 
+	public String insertPaymentDetails(String customerID, String customerName, String paymentType, String cardNo, String amount, String date, String billNo)
+	{
+		String output = "";
+		
+		try
+		{
+			Connection con = connect(); 
+			 if (con == null) 
+			 { 
+				 return "Error while connecting to the database for inserting."; 
+			 } 
+		   
+			 // create a prepared statement
+			 String query = " insert into payment (`paymentNo`,`customerID`,`customerName`,`paymentType`,`cardNo`,`paymentAmount`,`paymentDate`,`billNo`)"
+			 + " values (?, ?, ?, ?, ?, ?, ?, ?)";
+			 
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, customerID);
+			preparedStmt.setString(3, customerName);
+			preparedStmt.setString(4, paymentType);
+			preparedStmt.setString(5, cardNo);
+			preparedStmt.setDouble(6, Double.parseDouble(amount));
+			preparedStmt.setString(7, date);
+			preparedStmt.setString(8, billNo);
+			preparedStmt.execute();
+			con.close();
+			//output = "Payment detail inserted successfully";
+			String newPayment = readPaymentDetails(); 
+			 output = "{\"status\":\"success\", \"data\": \"" + 
+			 newPayment + "\"}"; 
+		}catch (Exception e)
+		 {
+			 //output = "Error while inserting the payment detail.";
+			 output = "{\"status\":\"error\", \"data\":\"Error while inserting the item.\"}";
+			 System.err.println(e.getMessage());
+		 }
+		    return output;
+	}
 	
 }
